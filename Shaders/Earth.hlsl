a22a73a4e92d9be36abc9abecd00f8b89ba48a89
Winbringer <voin1213@yandex.ru>
2016-03-25 15:42:18 +0600

    Добавил описание константных буфферов в шейдер

--- Shaders/Earth.hlsl
+++ Shaders/Earth.hlsl
@@ -1,25 +1,47 @@
 ﻿cbuffer data : register(b0)
 {
-    float4x4 WVP;
+    float4x4 WorldViewProjection;
+    float4x4 World;
+    float4x4 WorldInverseTranspose;
 };
+
+cbuffer data1 : register(b1)
+{
+    float Ns_SpecularPower;
+    float Ni_OpticalDensity;
+    float d_Transparency;
+    float Tr_Transparency;
+    float3 Tf_TransmissionFilter;  
+    float4 Ka_AmbientColor;
+    float4 Kd_DiffuseColor;
+    float4 Ks_SpecularColor;
+    float4 Ke_EmissiveColor;
+};
+
+cbuffer data2 : register(b2)
+{
+    float4 Color;
+   float3 Direction;
+};  
+
 struct VS_IN
 {
     float4 position : SV_Position;
-    float4 normal : NORMAL;
-    float4 textureUV : TEXCOORD;
+    float3 normal : NORMAL;
+    float3 textureUV : TEXCOORD;
 };
 
 struct PS_IN
 {
     float4 position : SV_Position;
-    float4 normal : NORMAL;
-    float4 textureUV : TEXCOORD;
+    float3 normal : NORMAL;
+    float3 textureUV : TEXCOORD;
 };
 
 PS_IN VS(VS_IN input)
 {
-    PS_IN output = (PS_IN) 0;
-    output.position = mul(input.position, WVP);   
+    PS_IN output = (PS_IN) 0;   
+    output.position = mul(input.position, WorldViewProjection);
     output.normal = input.normal;
     output.textureUV = input.normal;
 
